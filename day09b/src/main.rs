04c6a72d7bcbb27437e7b7facac715da8609580b
Graham Hoyes <accounts@grahamhoyes.com>
2024-12-09 17:17:32 -0500

    Day 9 part 2

--- day09b/src/main.rs
+++ day09b/src/main.rs
@@ -1,96 +1,7 @@
-use std::fmt::{Display, Formatter};
-use std::rc::Rc;
-
 #[derive(Debug, Clone)]
 enum Block {
     Free { size: u8 },
     File { id: usize, size: u8 },
-}
-
-impl Display for Block {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Block::Free { size } => {
-                write!(f, "{}", ".".repeat(*size as usize))
-            }
-            Block::File { id, size } => {
-                write!(f, "{}", id.to_string().repeat(*size as usize))
-            }
-        }
-    }
-}
-
-struct BlockNode {
-    block: Block,
-    next: Option<Rc<BlockNode>>,
-}
-
-impl BlockNode {
-    /// Split a given node at the given size
-    ///
-    /// Returns a new node with size `size`, which points to the new free
-    /// node. Returns None if the split is invalid.
-    fn split(&mut self, size: u8) -> Option<()> {
-        match &mut self.block {
-            Block::File { .. } => None,
-            Block::Free { size: free_size } if *free_size <= size => None,
-            Block::Free { size: free_size } => {
-                let remaining_size = *free_size - size;
-                *free_size = size;
-
-                let new_block = Block::Free {
-                    size: remaining_size,
-                };
-                let new_next = self.next.take();
-                let new_node = BlockNode {
-                    block: new_block,
-                    next: new_next,
-                };
-                self.next = Some(Rc::new(new_node));
-                Some(())
-            }
-        }
-    }
-}
-
-struct BlockList {
-    head: Option<Rc<BlockNode>>,
-    size: usize,
-}
-
-impl BlockList {
-    fn from_vec(blocks: Vec<Block>) -> Self {
-        let mut blocks_iter = blocks.into_iter();
-        let mut size = 0;
-
-        let head = blocks_iter.next().map(|first_block| {
-            size += 1;
-            let mut current = Rc::new(BlockNode {
-                block: first_block,
-                next: None,
-            });
-
-            let head = Rc::clone(&current);
-
-            for block in blocks_iter {
-                size += 1;
-                let new_node = Rc::new(BlockNode { block, next: None });
-                *current.next = Some(new_node);
-                current = current.next.unwrap();
-            }
-
-            head
-        });
-
-        BlockList { head, size }
-    }
-}
-
-fn print_blocks(blocks: &[Block]) {
-    for block in blocks {
-        print!("{}", block);
-    }
-    println!();
 }
 
 fn solution(input: &str) -> usize {
@@ -119,38 +30,50 @@ fn solution(input: &str) -> usize {
     // that actually compiles.
 
     let mut read_pos = blocks.len() - 1;
+    let mut max_id = blocks.len() - 1;
 
     while read_pos > 0 {
         match blocks[read_pos] {
-            Block::Free { .. } => {
-                read_pos -= 1;
-            }
+            Block::Free { .. } => {}
+            Block::File { id, .. } if id > max_id => {}
             Block::File { id, size } => {
                 // Search from the very beginning to see if there's a free block
                 // big enough to hold this
-                for write_pos in 0..blocks.len() {
+                for write_pos in 0..read_pos {
                     match blocks[write_pos] {
                         Block::File { .. } => continue,
                         Block::Free { size: free_size } => {
+                            #[allow(clippy::comparison_chain)]
                             if free_size == size {
                                 blocks[write_pos] = Block::File { size, id };
+                                blocks[read_pos] = Block::Free { size };
                                 break;
-                            } else if free_size < size {
-                                continue;
-                            } else {
+                            } else if free_size > size {
+                                blocks[read_pos] = Block::Free { size };
+
                                 let moved_block = Block::File { id, size };
                                 let free_block = Block::Free {
                                     size: free_size - size,
                                 };
 
                                 // Need to split the block. Here's the super inefficient part!
-                                blocks.splice(write_pos..write_pos, [moved_block, free_block]);
+                                blocks.splice(write_pos..=write_pos, [moved_block, free_block]);
+
+                                // And since we extended the size by one, scoot the read pointer over
+                                // to where it should be
+                                read_pos += 1;
+                                break;
+                            } else {
+                                continue;
                             }
                         }
                     }
                 }
+                max_id = id;
             }
         }
+
+        read_pos -= 1;
     }
 
     let mut checksum = 0usize;
@@ -164,8 +87,8 @@ fn solution(input: &str) -> usize {
                 }
                 block_idx += size as usize;
             }
-            Block::Free { .. } => {
-                break;
+            Block::Free { size } => {
+                block_idx += size as usize;
             }
         }
     }
@@ -174,7 +97,7 @@ fn solution(input: &str) -> usize {
 }
 
 fn main() {
-    let input = include_str!("../example.txt");
+    let input = include_str!("../input.txt");
     let res = solution(input);
 
     println!("Result: {}", res);
@@ -197,6 +120,6 @@ mod tests {
         let input = include_str!("../input.txt");
         let res = solution(input);
 
-        assert_eq!(res, 6378826667552);
+        assert_eq!(res, 6413328569890);
     }
 }
