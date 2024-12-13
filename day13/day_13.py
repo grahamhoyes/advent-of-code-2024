fba84baa343e3eef5a48372d64a82b0f4f579d90
Graham Hoyes <accounts@grahamhoyes.com>
2024-12-13 09:36:28 -0500

    Day 13 part 2 attempt with ilp

--- day13/day_13.py
+++ day13/day_13.py
@@ -28,19 +28,24 @@ def solve_machine(a_vec, b_vec, prize_vec):
     constraints = LinearConstraint(A, b_l, b_u)
 
     res = milp(c=c, constraints=constraints, integrality=integrality)
-    return res
+    return res.fun
 
 
-def solution(input: str) -> int:
+def solution(input: str, offset: int = 0) -> int:
     machines = input.split("\n\n")
     total_tokens = 0
 
-    for machine in machines:
+    for i, machine in enumerate(machines):
         a, b, prize = parse_machine(machine)
+
+        # Apply offset if needed
+        prize = tuple(p + offset for p in prize)
+
         res = solve_machine(a, b, prize)
 
-        if res.fun is not None:
-            total_tokens += int(res.fun)
+        if res is not None:
+            print(f"{i} is solvable")
+            total_tokens += int(res)
 
     return total_tokens
 
@@ -56,6 +61,10 @@ class TestSolution(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "1":
-        with open(f"{sys.argv[2]}.txt", "r") as fd:
-            print(solution(fd.read()))
+    if sys.argv[1] == "2":
+        offset = 10_000_000_000_000
+    else:
+        offset = 0
+
+    with open(f"{sys.argv[2]}.txt", "r") as fd:
+        print(solution(fd.read(), offset))
