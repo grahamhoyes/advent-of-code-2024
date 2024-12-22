9e6a09b32c36678ad7e779aaed65990744b1d90a
Graham Hoyes <accounts@grahamhoyes.com>
2024-12-22 09:37:13 -0500

    Day 22 Part 2

--- day22/src/part_1.rs
+++ day22/src/part_1.rs
@@ -1,19 +1,17 @@
 const PRUNE_MASK: u64 = (1 << 24) - 1; // % 16777216
 
-/// Iterate a secret value for a number of steps
+/// Iterate a secret value
 ///
 /// prng secrets are computed by an LFSR
-pub fn iterate_secret(mut secret: u64, steps: usize) -> u64 {
-    for _ in 0..steps {
-        secret ^= secret << 6; // * 64
-        secret &= PRUNE_MASK;
+pub fn iterate_secret(mut secret: u64) -> u64 {
+    secret ^= secret << 6; // * 64
+    secret &= PRUNE_MASK;
 
-        secret ^= secret >> 5; // / 32
-        secret &= PRUNE_MASK;
+    secret ^= secret >> 5; // / 32
+    secret &= PRUNE_MASK;
 
-        secret ^= secret << 11; // * 1024
-        secret &= PRUNE_MASK;
-    }
+    secret ^= secret << 11; // * 1024
+    secret &= PRUNE_MASK;
 
     secret
 }
@@ -22,7 +20,12 @@ pub fn solution(input: &str) -> u64 {
     input
         .lines()
         .map(|l| l.parse::<u64>().unwrap())
-        .map(|secret| iterate_secret(secret, 2000))
+        .map(|mut secret| {
+            for _ in 0..2000 {
+                secret = iterate_secret(secret)
+            }
+            secret
+        })
         .sum()
 }
 
@@ -35,7 +38,7 @@ mod tests {
         let input = include_str!("../example.txt");
         let res = solution(input);
 
-        assert_eq!(res, 37327623);
+        assert_eq!(res, 37990510);
     }
 
     #[test]
--- day22/src/part_2.rs
+++ day22/src/part_2.rs
@@ -1,5 +1,61 @@
-pub fn solution(_input: &str) -> u64 {
-    0
+use crate::part_1::iterate_secret;
+use std::collections::{HashMap, HashSet};
+
+/// Get the possible sequences of changes and their corresponding prices
+pub fn get_sequence_prices(mut secret: u64) -> HashMap<[i8; 4], i8> {
+    let mut prices: Vec<i8> = Vec::with_capacity(2000);
+    let mut changes: Vec<i8> = Vec::with_capacity(2000);
+
+    let mut previous_price = (secret % 10) as i8;
+
+    for _ in 0..2000 {
+        secret = iterate_secret(secret);
+        let price = (secret % 10) as i8;
+        prices.push(price);
+
+        let change = price - previous_price;
+        changes.push(change);
+
+        previous_price = price;
+    }
+
+    let mut sequence_values: HashMap<[i8; 4], i8> = HashMap::new();
+
+    for i in 3..prices.len() {
+        let sequence: [i8; 4] = changes[i - 3..=i].try_into().unwrap();
+        let price = prices[i];
+
+        // Only track the first occurrence
+        sequence_values.entry(sequence).or_insert(price);
+    }
+
+    sequence_values
+}
+
+pub fn solution(input: &str) -> u64 {
+    let sequence_prices: Vec<HashMap<[i8; 4], i8>> = input
+        .lines()
+        .map(|line| line.parse::<u64>().unwrap())
+        .map(get_sequence_prices)
+        .collect();
+
+    // Union together all the possible sequences
+    let all_sequences: HashSet<[i8; 4]> = sequence_prices
+        .iter()
+        .flat_map(|prices| prices.keys().copied())
+        .collect();
+
+    // Find the sequence that results in the highest total price
+    all_sequences
+        .into_iter()
+        .map(|sequence| {
+            sequence_prices
+                .iter()
+                .map(move |price_map| *price_map.get(&sequence).unwrap_or(&0) as u64)
+                .sum()
+        })
+        .max()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -11,7 +67,7 @@ mod tests {
         let input = include_str!("../example.txt");
         let res = solution(input);
 
-        assert_eq!(res, 0);
+        assert_eq!(res, 23);
     }
 
     #[test]
@@ -19,6 +75,6 @@ mod tests {
         let input = include_str!("../input.txt");
         let res = solution(input);
 
-        assert_eq!(res, 0);
+        assert_eq!(res, 1854);
     }
 }
