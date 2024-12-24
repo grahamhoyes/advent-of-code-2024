48668d87f535c9adf205145ceeccb98a429c4a2a
Graham Hoyes <accounts@grahamhoyes.com>
2024-12-24 17:28:56 -0500

    Day 24 part 2, by manual inspection

--- day24/src/part_1.rs
+++ day24/src/part_1.rs
@@ -1,8 +1,10 @@
 use itertools::Itertools;
 use std::collections::HashMap;
 use std::fmt::Formatter;
+use std::fs::File;
+use std::io::Write;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum Gate {
     And,
     Or,
@@ -23,6 +25,8 @@ impl std::fmt::Display for Gate {
 pub struct Network {
     pub values: HashMap<String, u8>,
     pub dependencies: HashMap<String, (String, Gate, String)>,
+    // Similar to dependencies, except the two strings are sorted
+    pub gates: HashMap<(String, Gate, String), String>,
 }
 
 impl Network {
@@ -38,7 +42,7 @@ impl Network {
             })
             .collect();
 
-        let dependencies = raw_network
+        let dependencies: HashMap<_, _> = raw_network
             .lines()
             .map(|line| {
                 let (op, output) = line.split_once(" -> ").unwrap();
@@ -56,9 +60,24 @@ impl Network {
             })
             .collect();
 
+        let gates = dependencies
+            .iter()
+            .map(|(output, (lhs, gate, rhs))| {
+                // Sort so the smallest one is first
+                let (lhs, rhs) = if lhs < rhs {
+                    (lhs.clone(), rhs.clone())
+                } else {
+                    (rhs.clone(), lhs.clone())
+                };
+
+                ((lhs, gate.clone(), rhs), output.clone())
+            })
+            .collect();
+
         Self {
             values,
             dependencies,
+            gates,
         }
     }
 
@@ -101,13 +120,42 @@ impl Network {
         None
     }
 
-    pub fn print_as_mermaid(&self) {
-        println!("graph TD;");
+    #[allow(dead_code)]
+    /// Write the network as a mermaid graph to a file
+    pub fn write_mermaid(&self, file: &str) -> std::io::Result<()> {
+        let mut file = File::create(file)?;
+
+        writeln!(file, "graph LR;")?;
+
+        let input_wires = self
+            .values
+            .keys()
+            .map(|key| key.to_string())
+            .sorted()
+            .collect_vec();
+
+        // Print each input node
+        for wire in input_wires {
+            if wire.starts_with("x") || wire.starts_with("y") {
+                // Inputs get a rounded rectangle
+                writeln!(file, "{}([{}]);", wire, wire)?;
+            }
+        }
 
         for (output, (lhs, gate, rhs)) in self.dependencies.iter() {
-            println!("{} -->|{}| {};", lhs, gate, output,);
-            println!("{} -->|{}| {};", rhs, gate, output,);
+            if output.starts_with("z") {
+                // Make final outputs look distinct
+                writeln!(file, "{}[[{} {} {} -> {}]]", output, lhs, gate, rhs, output)?;
+            } else {
+                writeln!(file, "{}({} {} {} -> {})", output, lhs, gate, rhs, output)?;
+            }
+
+            // Edges pointing in to the gate
+            writeln!(file, "{} --> {}", lhs, output)?;
+            writeln!(file, "{} --> {}", rhs, output)?;
         }
+
+        Ok(())
     }
 
     pub fn bits_to_val(&self, prefix: &str) -> u64 {
@@ -120,6 +168,7 @@ impl Network {
     }
 
     /// Get the x and y inputs
+    #[allow(dead_code)]
     pub fn get_inputs(&self) -> (u64, u64) {
         (self.bits_to_val("x"), self.bits_to_val("y"))
     }
@@ -127,6 +176,46 @@ impl Network {
     /// Get the output from the z wires
     pub fn get_output(&self) -> u64 {
         self.bits_to_val("z")
+    }
+
+    pub fn set_input(&mut self, name: &str, value: u64) {
+        for (key, val) in self.values.iter_mut() {
+            if key.starts_with(name) {
+                // Strip the prefix
+                let bit_place = key.strip_prefix(name).unwrap().parse::<usize>().unwrap();
+
+                *val = ((value >> bit_place) & 1) as u8;
+            }
+        }
+    }
+
+    /// Swap the output wires of two gates
+    pub fn swap_outputs(&mut self, wire1: &str, wire2: &str) {
+        let a = self.dependencies.get(wire1).unwrap().clone();
+        let b = self.dependencies.get(wire2).unwrap().clone();
+
+        self.dependencies.insert(wire1.to_string(), b.clone());
+        self.dependencies.insert(wire2.to_string(), a.clone());
+
+        // Swap the gates too, getting the order right
+        self.set_gate(&a.0, &a.2, a.1, wire2);
+        self.set_gate(&b.0, &b.2, b.1, wire1);
+    }
+
+    pub fn find_gate(&self, a: &str, b: &str, gate: Gate) -> Option<String> {
+        // Sort the inputs to match how we store them
+        let (a, b) = if a < b { (a, b) } else { (b, a) };
+
+        self.gates
+            .get(&(a.to_string(), gate, b.to_string()))
+            .cloned()
+    }
+
+    pub fn set_gate(&mut self, a: &str, b: &str, gate: Gate, output: &str) {
+        let (a, b) = if a < b { (a, b) } else { (b, a) };
+
+        self.gates
+            .insert((a.to_string(), gate, b.to_string()), output.to_string());
     }
 }
 
--- day24/src/part_2.rs
+++ day24/src/part_2.rs
@@ -1,72 +1,142 @@
 use crate::part_1::{Gate, Network};
 use itertools::Itertools;
 
+pub enum AdderValidation {
+    Valid { sum: String, carry: String },
+    Invalid { gate: String },
+}
+
+/// Validate that inputs a, b, and c_in are tied to a correct full adder.
+///
+/// Outputs the name of the sum output and the carry output.
+pub fn validate_full_adder(network: &Network, a: String, b: String, c_in: &str) -> AdderValidation {
+    println!("Validating full adder: {} + {} + {}", a, b, c_in);
+    // Sum output bit
+    let Some(first_xor_output) = network.find_gate(&a, &b, Gate::Xor) else {
+        return AdderValidation::Invalid {
+            gate: "Sum first XOR gate".to_string(),
+        };
+    };
+
+    let Some(sum_output) = network.find_gate(&first_xor_output, c_in, Gate::Xor) else {
+        return AdderValidation::Invalid {
+            gate: "Sum second XOR gate".to_string(),
+        };
+    };
+
+    // Carry block
+    let Some(first_and) = network.find_gate(&first_xor_output, c_in, Gate::And) else {
+        return AdderValidation::Invalid {
+            gate: "Carry first AND gate".to_string(),
+        };
+    };
+
+    let Some(second_and) = network.find_gate(&a, &b, Gate::And) else {
+        return AdderValidation::Invalid {
+            gate: "Carry second AND gate".to_string(),
+        };
+    };
+
+    let Some(carry_output) = network.find_gate(&first_and, &second_and, Gate::Or) else {
+        return AdderValidation::Invalid {
+            gate: format!("Carry OR gate. Expecting {} OR {}", first_and, second_and),
+        };
+    };
+
+    AdderValidation::Valid {
+        sum: sum_output.clone(),
+        carry: carry_output.clone(),
+    }
+}
+
+/// Validate that inputs a and b are tied to a correct half adder.
+///
+/// Outputs the name of the sum output and the carry output.
+pub fn validate_half_adder(network: &Network, a: &str, b: &str) -> Option<(String, String)> {
+    let sum_output = network
+        .find_gate(a, b, Gate::Xor)
+        .expect("Failed to find sum output");
+    let carry_output = network
+        .find_gate(a, b, Gate::And)
+        .expect("Failed to find carry output");
+
+    Some((sum_output, carry_output))
+}
+
+/// This is not an automated solution.
+///
+/// It relies on the fact that the actual input (but not the example) is structured
+/// as a sequence of full adders. Using the above validation functions, we can walk
+/// through the adder network and validate that each stage is structured properly.
+///
+/// When an invalid stage is detected, we break and print the gate that failed.
+/// By inspecting the stage in the printed mermaid diagram, we can pretty easily
+/// see which gates need to be swapped. Those gates are added to `to_swap`, repeating
+/// manually until the full network is valid.
 pub fn solution(input: &str) -> String {
-    let original_network = Network::from_input(input);
+    let mut network = Network::from_input(input);
 
-    let (x, y) = original_network.get_inputs();
-    let target = x + y;
+    let to_swap = [
+        ("z10", "kmb"),
+        ("z15", "tvp"),
+        ("dpg", "z25"),
+        ("vdk", "mmf"),
+    ];
 
-    // All possible pairs of wires that could be swapped
-    let wire_pairs: Vec<(&String, &String)> = original_network
-        .dependencies
-        .keys()
-        .tuple_combinations()
-        .collect();
+    for (wire1, wire2) in to_swap {
+        network.swap_outputs(wire1, wire2);
+    }
 
-    for (i, eight_wires) in original_network
-        .dependencies
-        .keys()
-        .combinations(8)
-        .enumerate()
-    {
-        let mut network = original_network.clone();
+    network
+        .write_mermaid("diagram.mermaid")
+        .expect("Failed to write mermaid file");
 
-        for i in (0..7).step_by(2) {
-            let wire1 = eight_wires[i];
-            let wire2 = eight_wires[i + 1];
+    // Traverse the network, validating that it makes a proper adder
+    let (_sum, mut carry) =
+        validate_half_adder(&network, "x00", "y00").expect("Initial half adder is invalid");
 
-            let a = network.dependencies.get_mut(wire1).unwrap() as *mut _;
-            let b = network.dependencies.get_mut(wire2).unwrap() as *mut _;
+    // Loop, validating full adders
+    for i in 1..45 {
+        let a = format!("x{:0>2}", i);
+        let b = format!("y{:0>2}", i);
 
-            unsafe {
-                std::ptr::swap(a, b);
+        match validate_full_adder(&network, a, b, &carry) {
+            AdderValidation::Valid {
+                carry: new_carry, ..
+            } => {
+                carry = new_carry;
             }
-        }
-
-        if let Some(output) = network.evaluate(100) {
-            if output == target {
-                let swapped = eight_wires.into_iter().sorted().collect_vec();
-
-                return swapped.into_iter().sorted().join(",");
+            AdderValidation::Invalid { gate } => {
+                panic!("Failed to validate full adder at bit {}: {}", i, gate);
             }
-        }
-
-        if i % 1_000 == 0 {
-            println!("{}: {:?}", i, eight_wires);
         }
     }
 
-    "".to_string()
+    to_swap
+        .iter()
+        .flat_map(|(a, b)| [a.to_string(), b.to_string()])
+        .sorted()
+        .join(",")
+        .to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_example() {
-        let input = include_str!("../example.txt");
-        let res = solution(input);
-
-        assert_eq!(res, "");
-    }
+    // #[test]
+    // fn test_example() {
+    //     let input = include_str!("../example.txt");
+    //     let res = solution(input);
+    //
+    //     assert_eq!(res, "");
+    // }
 
     #[test]
     fn test_input() {
         let input = include_str!("../input.txt");
         let res = solution(input);
 
-        assert_eq!(res, "");
+        assert_eq!(res, "dpg,kmb,mmf,tvp,vdk,z10,z15,z25");
     }
 }
